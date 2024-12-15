3c1a7fd0ffb0e362429f361ee4343dcba277a56a
edy <eduardosamsung2010@gmail.com>
2024-12-15 10:37:35 +0200

    wip project

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const hero1 = document.querySelector(".hero1");
 const hero2 = document.querySelector(".hero2");
 const healthBarHero1 = document.querySelector(".health-bar-hero1");
 const healthBarHero2 = document.querySelector(".health-bar-hero2");
+const progressBarHero1 = document.querySelector(".progress-bar-hero1");
+const progressBarHero2 = document.querySelector(".progress-bar-hero2");
 
 const hero1Move = [
   "./images/hero1/move_1.png",
@@ -29,6 +31,17 @@ const hero2Punch = [
   "./images/hero2/punch3.png",
   "./images/hero2/frame_1.png",
 ];
+const hero2Kill = [
+  "./images/hero2/kill1.png",
+  "./images/hero2/kill2.png",
+  "./images/hero2/kill3.png",
+];
+
+const hero1Kill = [
+  "./images/hero1/kill1.png",
+  "./images/hero1/kill2.png",
+  "./images/hero1/kill3.png",
+];
 
 hero1.style.left = "50px";
 hero2.style.left = "650px";
@@ -36,6 +49,8 @@ healthBarHero1.style.left = "100px";
 healthBarHero1.style.bottom = "270px";
 healthBarHero2.style.left = "700px";
 healthBarHero2.style.bottom = "270px";
+progressBarHero1.style.width = "116px";
+progressBarHero2.style.width = "116px";
 let hero1Touch = false;
 let hero2Touch = false;
 
@@ -113,42 +128,75 @@ const heroTouch = (direction, heroTouch) => {
   }
 };
 
+const hitHero = (progressBarHero, healthBarHero, hero, heroKill) => {
+  if (progressBarHero.style.width === "8.4px") {
+    progressBarHero.style.width = "0px";
+    healthBarHero.style.visibility = "hidden";
+    for (let i = 0; i < heroKill.length; i++) {
+      setTimeout(() => {
+        hero.src = heroKill[i];
+      }, 130 * i);
+    }
+  } else {
+    progressBarHero.style.width =
+      parseInt(progressBarHero.style.width) - 11.6 + "px";
+    hero.style.filter = "sepia(1) hue-rotate(-30deg) saturate(7)";
+    setTimeout(() => {
+      hero.style.filter = "";
+    }, 150);
+  }
+};
+
 window.addEventListener("keydown", (event) => {
-  if (event.key === "d") {
+  if (event.key === "d" && parseInt(progressBarHero1.style.width) !== 0) {
     if (parseInt(hero1.style.left) <= 680) {
       heroMovement("right", hero1, hero1Move);
       healthBarMove(healthBarHero1, "right", hero1);
       heroTouch("right", "hero1");
     }
   }
-  if (event.key === "a") {
+  if (event.key === "a" && parseInt(progressBarHero1.style.width) !== 0) {
     if (parseInt(hero1.style.left) >= 50) {
       heroMovement("left", hero1, hero1Move);
       healthBarMove(healthBarHero1, "left", hero1);
       heroTouch("left", "hero1");
     }
   }
-  if (event.key === "ArrowLeft") {
+  if (
+    event.key === "ArrowLeft" &&
+    parseInt(progressBarHero2.style.width) !== 0
+  ) {
     if (parseInt(hero2.style.left) >= 50) {
       heroMovement("left", hero2, hero2Move);
       healthBarMove(healthBarHero2, "left", hero2);
       heroTouch("right", "hero2");
     }
   }
-  if (event.key === "ArrowRight") {
+  if (
+    event.key === "ArrowRight" &&
+    parseInt(progressBarHero2.style.width) !== 0
+  ) {
     if (parseInt(hero2.style.left) <= 680) {
       heroMovement("right", hero2, hero2Move);
       healthBarMove(healthBarHero2, "right", hero2);
       heroTouch("left", "hero2");
     }
   }
-  if (event.key === "c") {
+  if (event.key === "c" && parseInt(progressBarHero1.style.width) !== 0) {
     heroPunchAnimation(hero1, hero1Punch);
-    console.log("hero1Touch", hero1Touch);
-    console.log("hero2Touch", hero2Touch);
+    if (hero1Touch === true || hero2Touch === true) {
+      healthBarHero2.style.zIndex = "2";
+      healthBarHero1.style.zIndex = "1";
+      hitHero(progressBarHero2, healthBarHero2, hero2, hero2Kill);
+    }
   }
-  if (event.key === "m") {
+  if (event.key === "m" && parseInt(progressBarHero2.style.width) !== 0) {
     heroPunchAnimation(hero2, hero2Punch);
+    if (hero2Touch === true || hero1Touch === true) {
+      healthBarHero2.style.zIndex = "1";
+      healthBarHero1.style.zIndex = "2";
+      hitHero(progressBarHero1, healthBarHero1, hero1, hero1Kill);
+    }
   }
 });
 
