385e84fff758cc78582e7fa5c381d2f947da2873
edy <eduardosamsung2010@gmail.com>
2025-02-16 10:41:43 +0200

    add restriction buttons

--- mechanics.js
+++ mechanics.js
@@ -1,38 +1,39 @@
-import dataGame from "./dataGame.js";
-const { hero1Touch, hero2Touch, hero1, hero2 } = dataGame;
+const hero1 = document.querySelector(".hero1");
+const hero2 = document.querySelector(".hero2");
 
 const heroTouch = (direction, heroTouch) => {
   const touch = parseInt(hero2.style.left) - parseInt(hero1.style.left);
 
   if (touch <= 110 && touch > -30 && direction === "right") {
     if (heroTouch === "hero1") {
-      dataGame.hero1Touch = true;
+      return true;
     }
     if (heroTouch === "hero2") {
-      dataGame.hero2Touch = true;
+      return true;
     }
   } else if (direction === "right") {
     if (heroTouch === "hero1") {
-      dataGame.hero1Touch = false;
+      return false;
     }
     if (heroTouch === "hero2") {
-      dataGame.hero2Touch = false;
+      return false;
     }
+
   }
 
   if (touch < 40 && touch >= -100 && direction === "left") {
     if (heroTouch === "hero1") {
-      dataGame.hero1Touch = true;
+      return true;
     }
     if (heroTouch === "hero2") {
-      dataGame.hero2Touch = true;
+      return true;
     }
   } else if (direction === "left") {
     if (heroTouch === "hero1") {
-      dataGame.hero1Touch = false;
+      return false;
     }
     if (heroTouch === "hero2") {
-      dataGame.hero2Touch = false;
+      return false;
     }
   }
 };
--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 import { heroPunchAnimation } from "./animations.js";
 import { heroMovement, healthBarMove } from "./movement.js";
 import { heroTouch } from "./mechanics.js";
-import dataGame from "./dataGame.js";
-const {
-  hero1,
-  backgroundSound,
-  switchSoundButton,
-  hero2,
-  healthBarHero1,
-  healthBarHero2,
-  progressBarHero1,
-  progressBarHero2,
-  countdown,
-  startGameLayout,
-  startBtn,
-  hero1Touch,
-  hero2Touch,
-} = dataGame;
 
-// const backgroundSound = document.querySelector(".my-audio");
-// const switchSoundButton = document.querySelector(".switch-sound");
-// const hero1 = document.querySelector(".hero1");
-// const hero2 = document.querySelector(".hero2");
-// const healthBarHero1 = document.querySelector(".health-bar-hero1");
-// const healthBarHero2 = document.querySelector(".health-bar-hero2");
-// const progressBarHero1 = document.querySelector(".progress-bar-hero1");
-// const progressBarHero2 = document.querySelector(".progress-bar-hero2");
-// const countdown = document.querySelector(".countdown");
-// const startGameLayout = document.querySelector(".start-game-layout");
-// const startBtn = document.querySelector(".start-btn");
+const backgroundSound = document.querySelector(".my-audio");
+const switchSoundButton = document.querySelector(".switch-sound");
+const hero1 = document.querySelector(".hero1");
+const hero2 = document.querySelector(".hero2");
+const healthBarHero1 = document.querySelector(".health-bar-hero1");
+const healthBarHero2 = document.querySelector(".health-bar-hero2");
+const progressBarHero1 = document.querySelector(".progress-bar-hero1");
+const progressBarHero2 = document.querySelector(".progress-bar-hero2");
+const countdown = document.querySelector(".countdown");
+const startGameLayout = document.querySelector(".start-game-layout");
+const startBtn = document.querySelector(".start-btn");
 
 hero1.style.left = "50px";
 hero2.style.left = "650px";
@@ -39,8 +23,8 @@ healthBarHero2.style.bottom = "270px";
 progressBarHero1.style.width = "116px";
 progressBarHero2.style.width = "116px";
 countdown.style.transform = "translate(-50%,-50%)";
-// let hero1Touch = false;
-// let hero2Touch = false;
+let hero1Touch = false;
+let hero2Touch = false;
 let countdownNumber = 3;
 let gameOn = false;
 
@@ -73,26 +57,33 @@ const hero2Kill = [
   "./images/hero2/kill2.png",
   "./images/hero2/kill3.png",
 ];
-
 const hero1Kill = [
   "./images/hero1/kill1.png",
   "./images/hero1/kill2.png",
   "./images/hero1/kill3.png",
 ];
-
 const hero1Kick = [
   "./images/hero1/kick1.png",
   "./images/hero1/kick2.png",
   "./images/hero1/kick3.png",
   "./images/hero1/frame_1.png",
 ];
-
 const hero2Kick = [
   "./images/hero2/kick1.png",
   "./images/hero2/kick2.png",
   "./images/hero2/kick3.png",
   "./images/hero2/Frame_1.png",
 ];
+const pressKeys = {
+  ms: true,
+  d: true,
+  a: true,
+  ArrowLeft: true,
+  ArrowRight: true,
+  c: true,
+  x: true,
+  n: true,
+};
 
 const hitHero = (progressBarHero, healthBarHero, hero, heroKill) => {
   if (progressBarHero.style.width === "8.4px") {
@@ -139,66 +130,12 @@ const startGame = () => {
 
 window.addEventListener("keydown", (event) => {
   if (
-    event.key === "d" &&
-    parseInt(progressBarHero1.style.width) !== 0 &&
-    gameOn
-  ) {
-    if (parseInt(hero1.style.left) <= 680) {
-      heroMovement("right", hero1, hero1Move);
-      healthBarMove(healthBarHero1, "right", hero1);
-      heroTouch("right", "hero1");
-    }
-  }
-  if (
-    event.key === "a" &&
-    parseInt(progressBarHero1.style.width) !== 0 &&
-    gameOn
-  ) {
-    if (parseInt(hero1.style.left) >= 50) {
-      heroMovement("left", hero1, hero1Move);
-      healthBarMove(healthBarHero1, "left", hero1);
-      heroTouch("left", "hero1");
-    }
-  }
-  if (
-    event.key === "ArrowLeft" &&
-    parseInt(progressBarHero2.style.width) !== 0 &&
-    gameOn
-  ) {
-    if (parseInt(hero2.style.left) >= 50) {
-      heroMovement("left", hero2, hero2Move);
-      healthBarMove(healthBarHero2, "left", hero2);
-      heroTouch("right", "hero2");
-    }
-  }
-  if (
-    event.key === "ArrowRight" &&
-    parseInt(progressBarHero2.style.width) !== 0 &&
-    gameOn
-  ) {
-    if (parseInt(hero2.style.left) <= 680) {
-      heroMovement("right", hero2, hero2Move);
-      healthBarMove(healthBarHero2, "right", hero2);
-      heroTouch("left", "hero2");
-    }
-  }
-  if (
-    event.key === "c" &&
-    parseInt(progressBarHero1.style.width) !== 0 &&
-    gameOn
-  ) {
-    heroPunchAnimation(hero1, hero1Punch);
-    if (hero1Touch === true || hero2Touch === true) {
-      healthBarHero2.style.zIndex = "2";
-      healthBarHero1.style.zIndex = "1";
-      hitHero(progressBarHero2, healthBarHero2, hero2, hero2Kill);
-    }
-  }
-  if (
     event.key === "m" &&
     parseInt(progressBarHero2.style.width) !== 0 &&
-    gameOn
+    gameOn &&
+    pressKeys.m
   ) {
+    pressKeys.m = false;
     heroPunchAnimation(hero2, hero2Punch);
     if (hero2Touch === true || hero1Touch === true) {
       healthBarHero2.style.zIndex = "1";
@@ -206,20 +143,92 @@ window.addEventListener("keydown", (event) => {
       hitHero(progressBarHero1, healthBarHero1, hero1, hero1Kill);
     }
   }
-  if (event.key === "x") {
+  if (
+    event.key === "d" &&
+    parseInt(progressBarHero1.style.width) !== 0 &&
+    gameOn &&
+    pressKeys.d
+  ) {
+    pressKeys.d = false;
+    if (parseInt(hero1.style.left) <= 680) {
+      heroMovement("right", hero1, hero1Move);
+      healthBarMove(healthBarHero1, "right", hero1);
+      hero1Touch = heroTouch("right", "hero1");
+    }
+  }
+  if (
+    event.key === "a" &&
+    parseInt(progressBarHero1.style.width) !== 0 &&
+    gameOn &&
+    pressKeys.a
+  ) {
+    pressKeys.a = false;
+    if (parseInt(hero1.style.left) >= 50) {
+      heroMovement("left", hero1, hero1Move);
+      healthBarMove(healthBarHero1, "left", hero1);
+      hero1Touch = heroTouch("left", "hero1");
+    }
+  }
+  if (
+    event.key === "ArrowLeft" &&
+    parseInt(progressBarHero2.style.width) !== 0 &&
+    gameOn &&
+    pressKeys.ArrowLeft
+  ) {
+    pressKeys.ArrowLeft = false;
+    if (parseInt(hero2.style.left) >= 50) {
+      heroMovement("left", hero2, hero2Move);
+      healthBarMove(healthBarHero2, "left", hero2);
+      hero2Touch = heroTouch("right", "hero2");
+    }
+  }
+  if (
+    event.key === "ArrowRight" &&
+    parseInt(progressBarHero2.style.width) !== 0 &&
+    gameOn &&
+    pressKeys.ArrowRight
+  ) {
+    pressKeys.ArrowRight = false;
+    if (parseInt(hero2.style.left) <= 680) {
+      heroMovement("right", hero2, hero2Move);
+      healthBarMove(healthBarHero2, "right", hero2);
+      hero2Touch = heroTouch("left", "hero2");
+    }
+  }
+  if (
+    event.key === "c" &&
+    parseInt(progressBarHero1.style.width) !== 0 &&
+    gameOn &&
+    pressKeys.c
+  ) {
+    pressKeys.c = false;
+    heroPunchAnimation(hero1, hero1Punch);
+    if (hero1Touch === true || hero2Touch === true) {
+      healthBarHero2.style.zIndex = "2";
+      healthBarHero1.style.zIndex = "1";
+      hitHero(progressBarHero2, healthBarHero2, hero2, hero2Kill);
+    }
+  }
+  if (event.key === "x" && pressKeys.x) {
+    pressKeys.x = false;
     for (let i = 0; i < hero1Kick.length; i++) {
       setTimeout(() => {
         hero1.src = hero1Kick[i];
       }, i * 80);
     }
   }
-  if (event.key === "n") {
+  if (event.key === "n" && pressKeys.n) {
+    pressKeys.n = false;
     for (let i = 0; i < hero2Kick.length; i++) {
       setTimeout(() => {
         hero2.src = hero2Kick[i];
       }, i * 80);
     }
   }
+});
+
+window.addEventListener("keyup", (event) => {
+  pressKeys[event.key] = true;
 });
 
 switchSoundButton.addEventListener("click", () => {
